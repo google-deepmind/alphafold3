dd6595cb36663db0efcc7d3431c1b50d7324b597
Dr Owain Kenway <o.kenway@ucl.ac.uk>
2025-07-14 15:30:19 +0100

    Suppress output from Jackhmmer check
    Prevent Jackhmmer printing many copies of its usage to the terminal when checking if it has been patched.

--- src/alphafold3/data/tools/subprocess_utils.py
+++ src/alphafold3/data/tools/subprocess_utils.py
@@ -39,7 +39,7 @@ def check_binary_exists(path: str, name: str) -> None:
 def jackhmmer_seq_limit_supported(jackhmmer_path: str) -> bool:
   """Checks if Jackhmmer supports the --seq-limit flag."""
   try:
-    subprocess.run([jackhmmer_path, '-h', '--seq_limit', '1'], check=True)
+    subprocess.run([jackhmmer_path, '-h', '--seq_limit', '1'], capture_output=True, check=True)
   except subprocess.CalledProcessError:
     return False
   return True
